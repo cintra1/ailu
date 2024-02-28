69f524ccec5d98a413be3fb392e7a1a461bcbe67
Mateus Cintra <mateusdade@gmail.com>
2024-02-28 20:13:02 -0300

    Refactor variable names and update comments

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,4 +1,6 @@
 /* eslint-disable camelcase */
+'use client';
+
 import { clerkClient } from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
