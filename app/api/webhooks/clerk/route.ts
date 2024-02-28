6bb5af58e38fa18424da661caea666aba3882009
Mateus Cintra <mateusdade@gmail.com>
2024-02-28 20:57:10 -0300

    Remove unused import and incorrect comment

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,4 @@
 /* eslint-disable camelcase */
-'use client';
-
 import { clerkClient } from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
