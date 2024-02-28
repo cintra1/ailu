7538c0bade0dbf3f1a3bbd4a66ac2af9f60c3606
Mateus Cintra <mateusdade@gmail.com>
2024-02-28 16:37:19 -0300

    Adicionando a fonte GeistSans no projeto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { GeistSans } from 'geist/font/sans';
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
-const IBMPlex = IBM_Plex_Sans({
-  subsets: ["latin"],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-ibm-plex'
-});
 
 export const metadata: Metadata = {
   title: "Ailu",
@@ -24,8 +19,8 @@ export default function RootLayout({
     <ClerkProvider appearance={{
       variables: { colorPrimary: '#624cf5'}
     }} >
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+    <html lang="en" className={GeistSans.className}>
+      <body>
         {children}
       </body>
       </html>
