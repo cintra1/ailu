9a282ea08508f7878d3c2f10a811e907afde54e2
Mateus Cintra <mateusdade@gmail.com>
2024-03-05 22:07:24 -0300

    Create Profile and Buy Credits page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,7 +14,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
   return (
     <>
       <section className="home">
-        <h1 className='home-heading'>Unleash Your Creative Vision with ailu</h1>
+        <h1 className='home-heading'>Unleash Your Creative Vision with Ailu</h1>
         <ul className='flex-center w-full gap-20'>
           {navLinks.slice(1, 5).map((link) => (
             <Link
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          port: ''
+        }
+      ]
+    }
+  };
+  
+  export default nextConfig;
